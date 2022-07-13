0c1c0c83b0626a56c38af5f9e0e0038523e94cf4
leusin <leusin@naver.com>
2022-07-14 01:28:21 +0900

    Calculate price from order details

--- cupcake-app-starter/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ cupcake-app-starter/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -2,9 +2,14 @@ package com.example.cupcake.model
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import java.text.NumberFormat
 import java.text.SimpleDateFormat
 import java.util.*
+
+private const val PRICE_PER_CUPCAKE = 2.00
+private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
 
 class OrderViewModel : ViewModel() {
 
@@ -17,11 +22,22 @@ class OrderViewModel : ViewModel() {
     private val _date = MutableLiveData<String>("")
     val date: LiveData<String> = _date
 
-    private val _price = MutableLiveData<Double>(0.0)
-    val price: LiveData<Double> = _price
+    private val _price = MutableLiveData<Double>()
+    val price: LiveData<String> = Transformations.map(_price) {
+        NumberFormat.getCurrencyInstance().format(it)
+    }
 
     fun setQuantity(numberCupcakes: Int) {
         _quantity.value = numberCupcakes
+        updatePrice()
+    }
+
+    private fun updatePrice() {
+        var calculatedPrice = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
+        if (dateOptions[0] == _date.value) {
+            calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP
+        }
+        _price.value = calculatedPrice
     }
 
     fun setFlavor(desiredFlavor: String) {
@@ -30,6 +46,7 @@ class OrderViewModel : ViewModel() {
 
     fun setDate(pickupDate: String) {
         _date.value = pickupDate
+        updatePrice()
     }
     // 컵케이크의 맛이 설정 되었는지 여부 확인
     fun hasNoFlavorSet(): Boolean {
@@ -59,5 +76,6 @@ class OrderViewModel : ViewModel() {
     init {
         resetOrder()
     }
+
 
 }
